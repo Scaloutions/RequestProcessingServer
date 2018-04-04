2ca5ef7d4da507d3a5f3c8422f16f03a358fece6
chrisann0295 <chrisann@uvic.ca>
2018-04-04 00:40:28 -0700

    fixing dumplog

--- server.go
+++ server.go
@@ -218,7 +218,7 @@ func startProcessingUser(userId string, RequestsQueue *RequestsQueue) {
 				"FileName":      userId,
 				"CommandNumber": currentRequest.CommandNumber}
 
-			handleHttpRequestResponse(requestBody, DISPLAY_SUMMARY, RequestsQueue, currentRequest.CommandNumber)
+			handleHttpRequestResponse(requestBody, DUMPLOG, RequestsQueue, currentRequest.CommandNumber)
 			break
 
 		}
